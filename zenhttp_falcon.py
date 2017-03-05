c669d813be5b745a07e607ce17c072262e9de3e8
Squeaky <squeaky_pl@gmx.com>
2017-03-04 23:48:34 -0300

    actual zenline code for falcon

--- zenhttp_falcon.py
+++ zenhttp_falcon.py
@@ -6,33 +6,28 @@ sys.path.insert(
 
 import falcon
 
+from zenlines import zenlines
+
 
 class ZenResource(object):
     def on_get(self, req, resp):
-        # marker = req.get_param('marker') or ''
-        # limit = req.get_param_as_int('limit') or 50
-        #
-        # try:
-        #     result = self.db.get_things(marker, limit)
-        # except Exception as ex:
-        #     self.logger.error(ex)
-        #
-        #     description = ('Aliens have attacked our base! We will '
-        #                    'be back as soon as we fight them off. '
-        #                    'We appreciate your patience.')
-        #
-        #     raise falcon.HTTPServiceUnavailable(
-        #         'Service Outage',
-        #         description,
-        #         30)
+        q = req.get_param('q')
 
-        # An alternative way of doing DRY serialization would be to
-        # create a custom class that inherits from falcon.Request. This
-        # class could, for example, have an additional 'doc' property
-        # that would serialize to JSON under the covers.
-        resp.body = 'Hello'
-        resp.set_header('Connection', 'Keep-Alive')
         resp.content_type = 'text/plain; charset=utf-8'
+
+        if not q:
+            resp.body = 'Bad Request'
+            resp.status = falcon.HTTP_400
+            return
+
+        try:
+            zenline = zenlines[q]
+        except KeyError:
+            resp.body = 'Not Found'
+            resp.status = falcon.HTTP_404
+            return
+
+        resp.body = zenline
         resp.status = falcon.HTTP_200
 
 
